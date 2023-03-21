e1e6f944d1d4ab22ae2c305e74b3e4d1afedf47b
osvaldoCesar <osva.ces.1222@gmail.com>
2023-03-20 20:09:54 -0600

    Se agrega ícono al sistema

--- resources/views/sections/head.blade.php
+++ resources/views/sections/head.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>Punto de venta Welldent</title>
-
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/plantilla.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
